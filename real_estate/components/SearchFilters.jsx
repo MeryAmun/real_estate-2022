78e46875e337d0162cb31218f95e4e4164510a4f
MeryAmun <chebesiawah@gmail.com>
2022-01-14 10:36:09 +0100

    no result

---
+++ real_estate/components/SearchFilters.jsx
@@ -0,0 +1,19 @@
+import {
+  Box,
+  Button,
+  Flex,
+  Icon,
+  Input,
+  Select,
+  Spinner,
+  Text,
+} from '@chakra-ui/react'
+import { useEffect, useState } from 'react'
+
+import Image from 'next/image'
+import { MdCancel } from 'react-icons/md'
+import { useRouter } from 'next/router'
+
+export const SearchFilters = () => {
+  return <div>SearchFilter</div>
+}
--- real_estate/pages/search.js
+++ real_estate/pages/search.js
@@ -2,7 +2,9 @@ import { Box, Flex, Icon, Text } from '@chakra-ui/react'
 
 import { BsFilter } from 'react-icons/bs'
 import Image from 'next/image'
-import SearchFilters from './SearchFilters'
+import { Property } from '../components/Property'
+import { SearchFilters } from '../components/SearchFilters'
+import noResult from '../assets/images/noresult.svg'
 import { useRouter } from 'next/router'
 import { useState } from 'react'
 
@@ -28,6 +30,25 @@ const Search = () => {
         <Icon paddingLeft='2' w='7' as={BsFilter} />
       </Flex>
       {searchFilters && <SearchFilters />}
+      <Text fontSize='2xl' p='4' fontWeight='bold'>
+        Properties {router.query.purpose}
+      </Text>
+      <Flex flexWrap='wrap'>
+        {[].map((property) => (
+          <Property property={property} key={property.id} />
+        ))}
+      </Flex>
+      {[].length === 0 && (
+        <Flex
+          justifyContent='center'
+          alignItems='center'
+          flexDirection='column'
+          marginBottom='5'
+          marginTop='5'
+        >
+          <Image src={noResult} alt='No Result' />
+        </Flex>
+      )}
     </Box>
   )
 }
