89f24246c198d69b8dc1f62c8a5d18c23456a043
MeryAmun <chebesiawah@gmail.com>
2022-01-14 12:04:50 +0100

    search ffilters options

--- real_estate/components/SearchFilters.jsx
+++ real_estate/components/SearchFilters.jsx
@@ -8,6 +8,7 @@ import {
   Spinner,
   Text,
 } from '@chakra-ui/react'
+import { filterData, getFilterValue } from '../utils/filterData.js.js'
 import { useEffect, useState } from 'react'
 
 import Image from 'next/image'
@@ -15,8 +16,29 @@ import { MdCancel } from 'react-icons/md'
 import { useRouter } from 'next/router'
 
 export const SearchFilters = () => {
-  const [filters, setFilters] = useState()
+  const [filters, setFilters] = useState(filterData)
+  const searchProperties = (filterValues) => {}
+
   return (
-    <Flex bg='gray.100' p='4' justifyContent='center' flexWrap='wrap'></Flex>
+    <Flex bg='gray.100' p='4' justifyContent='center' flexWrap='wrap'>
+      {filters.map((filter) => (
+        <Box key={filter.queryName}>
+          <Select
+            placeholder={filter.placeholder}
+            w='fit-content'
+            p='2'
+            onChange={(e) =>
+              searchProperties({ [filter.queryName]: e.target.value })
+            }
+          >
+            {filter?.items?.map((item) => (
+              <option value={item.value} key={item.value}>
+                {item.name}
+              </option>
+            ))}
+          </Select>
+        </Box>
+      ))}
+    </Flex>
   )
 }
