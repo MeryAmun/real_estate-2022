1993afc7a3d54cc3774072f7a1449fcd7e54a56d
MeryAmun <chebesiawah@gmail.com>
2022-01-14 11:42:29 +0100

    filter data file

--- real_estate/components/SearchFilters.jsx
+++ real_estate/components/SearchFilters.jsx
@@ -15,5 +15,8 @@ import { MdCancel } from 'react-icons/md'
 import { useRouter } from 'next/router'
 
 export const SearchFilters = () => {
-  return <div>SearchFilter</div>
+  const [filters, setFilters] = useState()
+  return (
+    <Flex bg='gray.100' p='4' justifyContent='center' flexWrap='wrap'></Flex>
+  )
 }
