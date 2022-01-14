99e34d2908379d0352f91c18cf85c98d5637ab7d
MeryAmun <chebesiawah@gmail.com>
2022-01-14 02:00:49 +0100

    provider

--- real_estate/pages/_app.js
+++ real_estate/pages/_app.js
@@ -1,5 +1,11 @@
 import '../styles/globals.css'
 
+import { ChakraProvider } from '@chakra-ui/react'
+import Head from 'next/head'
+import Layout from '../components/Layout'
+import NProgress from 'nprogress'
+import Router from 'next/router'
+
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
 }
